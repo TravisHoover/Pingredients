94d53370d996888f1c0177bba36ede8436ba5c4d
Travis Hoover <thoover@smartvue.com>
2017-09-16 12:20:40 -0500

    Added RecipeList component. This will fetch recipes eventually.

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import RecipeList from './src/components/RecipeList'
 
 const App = () => (
-  <Header headerText={'Pingredients'} />
+  <View>
+    <Header headerText={'Pingredients'} />
+    <RecipeList/>
+  </View>
 );
 
 
---
+++ src/components/RecipeList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RecipeList = () => {
+  return (
+    <View>
+      <Text>Recipe List</Text>
+    </View>
+  )
+};
+
+export default RecipeList;
