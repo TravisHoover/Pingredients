e5c9f1d885ae7bdb01b5d6bbe2b12f6b60b5ce04
Travis Hoover <traviskhoover@gmail.com>
2017-10-24 11:08:23 -0500

    Attach access_token to state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,44 +1,27 @@
 import React, { Component } from 'react';
-import { Text, View, Linking } from 'react-native'
-import firebase from 'firebase';
-import { Button, Card, CardSection, Input, Spinner } from './common';
-import OauthForm from './OauthForm';
+import { Linking } from 'react-native';
+import { Button, Card, CardSection, Spinner } from './common';
 
 class LoginForm extends Component {
-  state = {  email   : '',
-    password : '',
-    error    : '',
-    loading  : false,
-    login    : true,
-  };
+  constructor (props) {
+    super(props);
+    this.state = {
+      email   : '',
+      password : '',
+      access_token : '',
+      error    : '',
+      loading  : false,
+      login    : true,
+    };
+  }
   
-  onButtonPress() {
-    console.log('button pressed')
+  onButtonPress = () => {
     Linking.openURL('https://api.pinterest.com/oauth/?scope=read_public&client_id=4911971725215810382&state=768uyFys%20response_type=code&redirect_uri=https://pingredients&response_type=token')
-    Linking.addEventListener( 'url', handleUrl)
-    function handleUrl( event ) {
-      console.log('this handle url1 was called');
-      console.log('event: ', event);
+    Linking.addEventListener( 'url', handleUrl.bind(this))
+    function handleUrl(event) {
+      let accessToken = event.url.match(/\?(?:access_token)\=([\S\s]*?)\&/)[1];
+      this.setState({access_token: accessToken})
     }
-  }
-  
-  componentWillMount() {
-    console.log('componentWillMount called');
-    Linking.getInitialURL()
-    .then((ev) => {
-      if (ev) {
-        console.log('ev: ', ev);
-        this.handleUrl(ev);
-      }
-    }).catch(err => {
-      console.log('An error occurred', err);
-    });
-    Linking.addEventListener( 'url', this.handleUrl)
-  }
-  
-  handleUrl( event ) {
-    console.log('handleUrl2 called');
-    console.log('event: ', event);
   }
   
   renderButton() {
@@ -53,6 +36,7 @@ class LoginForm extends Component {
   }
   
   render() {
+    console.log('state: ', this.state);
     return (
       <Card>
         <CardSection>
